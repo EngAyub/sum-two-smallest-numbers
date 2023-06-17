7ba49f82d803771ee4342f75da776743e25f79b9
ENG AYUUB <moztaphem9@gmal.com>
2023-06-17 20:33:09 +0300

    Sum-two-smallest-numbers done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Good Luck 💪🏾
+
+function sumTwoSmallestNums(AK) {
+    AK.sort(function(c, d) {
+      return c - d;
+    });
+    var sum = AK[0] + AK[1];
+    return sum;
+  }
+  console.log(sumTwoSmallestNums([12, 5, 24, 55, 18]));//17
+  console.log(sumTwoSmallestNums([190, 300, 230, 800]));  420 //
+   
+
+
+
+  
